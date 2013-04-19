76d84f33c7d84765b81044ebdfaa83a135d5ab44
Robert Medeiros <robert@crimeminister.org>
2013-04-19 10:24:31 -0400

    Additional work on data models and corresponding admin.

--- myspacecal/myspacecal/data/admin.py
+++ myspacecal/myspacecal/data/admin.py
@@ -5,12 +5,32 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from models import Agency
 from models import Satellite
+from models import Target
+from models import Observation
 
 
 class AgencyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('acronym','name',)
 admin.site.register(Agency, AgencyAdmin)
 
 class SatelliteAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = (
+        'name',
+        'wavelength',
+        'agencies',
+        'operational',
+    )
+    def agencies(self, obj):
+       agency_list = [str(a) for a in obj.agency.all()]
+       return ", ".join(agency_list)
+    agencies.short_description = _("Agencies")
+
 admin.site.register(Satellite, SatelliteAdmin)
+
+class TargetAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Target, TargetAdmin)
+
+class ObservationAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Observation, ObservationAdmin)
--- myspacecal/myspacecal/data/models.py
+++ myspacecal/myspacecal/data/models.py
@@ -10,16 +10,21 @@ class Agency(models.Model):
     # Fields
     name = models.CharField(
         max_length = 100,
-        help_text = _("The agency name")
+        help_text = _("The full agency name")
+    )
+    acronym = models.CharField(
+        max_length = 10,
+        help_text = _("The agency acronym")
     )
     url = models.URLField(
         help_text = _("Agency home page")
     )
 
     def __unicode__(self):
-        return unicode(self.name)
+        return unicode(self.acronym)
 
     class Meta:
+        ordering = ('acronym',)
         verbose_name_plural = _("Agencies")
 
 
@@ -33,10 +38,32 @@ class Satellite(models.Model):
     )
     agency = models.ManyToManyField(
         Agency,
+        related_name = 'satellites',
         help_text = _("Agencies responsbile for the satellite")
+    )
+    WAVELENGTH_UNSPECIFIED = 0
+    WAVELENGTH_XRAY = 1
+    WAVELENGTH_GAMMA_RAY = 2
+    WAVELENGTH_INFRARED = 3
+    WAVELENGTH_MULTI = 4
+    WAVELENGTH = (
+        (WAVELENGTH_UNSPECIFIED, _("Unspecified")),
+        (WAVELENGTH_XRAY, _("X-Ray")),
+        (WAVELENGTH_GAMMA_RAY, _("Gamma Ray")),
+        (WAVELENGTH_INFRARED, _("Infrared")),
+        (WAVELENGTH_MULTI, _("Multi-wavelength")),
+    )
+    wavelength = models.PositiveSmallIntegerField(
+        choices = WAVELENGTH,
+        default = WAVELENGTH_UNSPECIFIED,
+        help_text = _("Wavelength that satellite observes")
     )
     url = models.URLField(
         help_text = _("Satellite home page")
+    )
+    operational = models.BooleanField(
+        default = True,
+        help_text = _("Is the satellite mission still in-progress?")
     )
 
     def __unicode__(self):
@@ -55,11 +82,22 @@ class Target(models.Model):
         unique = True,
         help_text = _("The target name")
     )
-    right_ascension = models.FloatField(
-        help_text = _("Right ascension, radians")
+
+    # These fields are defined as xsd:decimal in the published schema
+    # of the existing app. Conforming implementations need to have
+    # minimum of 18 significant digits, but no maximum is specified.
+
+    ra = models.DecimalField(
+        max_digits = 32,
+        decimal_places = 8,
+        verbose_name = "RA",
+        help_text = _("Right ascension")
     )
-    declinaton = models.TimeField(
-        help_text = _("Declination, radians")
+    dec = models.DecimalField(
+        max_digits = 32,
+        decimal_places = 8,
+        verbose_name = "DEC",
+        help_text = _("Declination")
     )
 
     def __unicode__(self):
@@ -73,17 +111,20 @@ class Observation(models.Model):
     """
     """
     # Fields
+    id = models.PositiveIntegerField(
+        max_length = 50,
+        primary_key = True,
+        help_text = _("Observation ID")
+    )
     satellite = models.ForeignKey(
         Satellite,
+        related_name = 'observations',
         help_text = _("Satellite performing the observation")
     )
     target = models.ForeignKey(
         Target,
+        related_name = 'observations',
         help_text = _("Target being observed")
-    )
-    name = models.CharField(
-        max_length = 50,
-        help_text = _("")
     )
     revolution = models.PositiveIntegerField(
         help_text = _("Orbital revolution")
